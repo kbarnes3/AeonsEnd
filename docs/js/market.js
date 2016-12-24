f4aac061470f72fdba4c75b25aae8355f520bc76
krenaud87 <kaitlinrenaud@gmail.com>
2016-12-23 16:00:47 -0800

    Fix market generation logic and sort cards. (#2)
    * Make cards a const.
    
    * Sort returned cards.
    
    * Fix spacing.
    
    * Generate market.js

--- docs/js/market.js
+++ docs/js/market.js
@@ -471,32 +471,49 @@ function itemMatchesCostPredicate(itemType, costPredicate) {
 "use strict";
 
 const markets = require('./all_markets.js');
-// Ideally this would be const
-var cards = require('./all_cards.js');
+const cards = require('./all_cards.js');
 
-function getMatchingCards(cardMatchFn, cards) {
+function getMatchingCards(cardMatchFn, deck, skipCards) {
     var matchingCards = [];
-    for (var i = 0; i < cards.length; i++) {
-        var card = cards[i];
+    for (var i = 0; i < deck.length; i++) {
+        var card = deck[i];
         if (cardMatchFn(card)) {
-            matchingCards.push(card);
+            if (skipCards.indexOf(card) == -1) {
+                matchingCards.push(card);
+            }
         }
     }
     return matchingCards;
 }
 
 function getCardsInMarket(market) {
-    var remainingCards = cards.ALL_CARDS;
+    var deck = cards.ALL_CARDS;
     var retCards = [];
     for (var i = 0; i < market.cards.length; i++) {
         var cardFn = market.cards[i];
-        var eligibleCards = getMatchingCards(cardFn, remainingCards);
+        var eligibleCards = getMatchingCards(cardFn, deck, retCards);
         var card = eligibleCards[randNumber(eligibleCards.length)];
         retCards.push(card);
-        var index = remainingCards.indexOf(card);
-        remainingCards.splice(index, 1);
     }
+    sortCards(retCards);
     return retCards;
+}
+
+// Sorts first by type, then cost, and finally by name.
+function sortCards(deck) {
+    var compareFn = function(c1, c2) {
+        var typeOrder = ['Gem', 'Relic', 'Spell'];
+        if (c1.type == c2.type) {
+            if (c1.cost == c2.cost) {
+                return c1.name.localeCompare(c2.name);
+            }
+            return c1.cost - c2.cost;
+        }
+        var c1TypeIndex = typeOrder.indexOf(c1.type);
+        var c2TypeIndex = typeOrder.indexOf(c2.type);
+        return c1TypeIndex - c2TypeIndex;
+    };
+    deck.sort(compareFn);
 }
 
 function randNumber(max) {
