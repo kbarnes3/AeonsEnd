9dead2c68330b4c1ffab8a511e6c9fff286ff423
Kevin Barnes <kbarnes3@gmail.com>
2017-01-28 11:19:02 -0800

    Market generation takes in an array of expansions

--- node/index.js
+++ node/index.js
@@ -1,4 +1,9 @@
 "use strict";
 
 var marketGenerator = require('./createMarket.js');
-console.log(marketGenerator.createRandomMarket());
+var all_expansions = [
+    'Base',
+    'The Depths',
+    'The Nameless',
+];
+console.log(marketGenerator.createRandomMarket(all_expansions));
