9415f761ca43ee4f30819fcd07bf9b0ec6203c60
Kevin Barnes <kbarnes3@gmail.com>
2019-01-06 17:46:45 -0800

    Removing old version

--- node/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var marketGenerator = require('./createMarket.js');
-var all_expansions = [
-    'Base',
-    'The Depths',
-    'The Nameless',
-];
-console.log(marketGenerator.createRandomMarket(all_expansions));
--- node/index_beta.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var marketGenerator = require('./createMarket.js');
-var beta_expansion = [
-    'War Eternal 1',
-];
-console.log(marketGenerator.createRandomMarket(beta_expansion));
